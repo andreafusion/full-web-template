8f632110e570305adebeeae1536b0d9bc581dc85
andreafusion <andreafusiondigital@gmail.com>
2026-02-18 21:49:28 +0100

    about final

--- src/data/services.ts
+++ src/data/services.ts
@@ -9,25 +9,25 @@ export interface Service {
 
 export const services: Service[] = [
   {
-    id:          'natural-clean',
-    name:        'Natural & Clean',
-    description: 'Manicura básica + esmaltado',
+    id:          'semipermanente + rusa',
+    name:        'Semipermanente & Manicura Rusa',
+    description: 'Cutícula pulida, color uniforme y acabado limpio',
     priceFrom:   18,
     duration:    '45 min',
     image:       '/assets/img/nails-06.webp',
   },
   {
-    id:          'cute-detail',
-    name:        'Cute Detail',
-    description: 'Diseños delicados personalizados',
+    id:          'nailart-iconic',
+    name:        'Nail Art Iconic',
+    description: 'Detalles, efectos, 3D, encapsulados… diseños personalizados',
     priceFrom:   25,
     duration:    '60 min',
     image:       '/assets/img/nails-04.webp',
   },
   {
     id:          'soft-glam',
-    name:        'Soft Glam',
-    description: 'Refuerzo / gel con acabado pulido',
+    name:        'Soft Gel | Refuerzo | Polygel',
+    description: 'Durabilidad extra y estructura bonita: uñas más resistentes, finas y con acabado pulido.',
     priceFrom:   30,
     duration:    '90 min',
     image:       '/assets/img/nails-07.webp',
